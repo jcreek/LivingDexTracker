4c9631e00f589a72ef0b1b26212a2905aa5ec195
Josh Creek <8179928+jcreek@users.noreply.github.com>
2024-04-10 21:50:43 +0100

    feat(*): Add custom pokeball theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,22 @@ export default {
 	},
 	plugins: [require('daisyui')],
 	daisyui: {
-		themes: ['light', 'dark', 'dracula']
+		themes: [
+			'light',
+			'dark',
+			'dracula',
+			{
+				pokeball: {
+					primary: '#ee1515',
+					'primary-content': '#ffffff',
+					secondary: '#3b82c4',
+					'secondary-content': '#ffffff',
+					accent: '#7e308e',
+					neutral: '#ffffff',
+					'base-100': '#f0f0f0',
+					'base-content': '#222224'
+				}
+			}
+		]
 	}
 };
