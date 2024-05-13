425d66a9ce20aaf6eb8c082018808e8d1e50f51b
Josh Creek <8179928+jcreek@users.noreply.github.com>
2024-05-13 19:32:24 +0100

    fix(*): Disable some functionality of service worker

--- src/prompt-sw.ts
+++ src/prompt-sw.ts
@@ -25,8 +25,8 @@ precacheAndRoute(self.__WB_MANIFEST);
 // clean old assets
 cleanupOutdatedCaches();
 
-let allowlist: undefined | RegExp[];
-if (import.meta.env.DEV) allowlist = [/^\/$/];
+// let allowlist: undefined | RegExp[];
+// if (import.meta.env.DEV) allowlist = [/^\/$/];
 
-// to allow work offline
-registerRoute(new NavigationRoute(createHandlerBoundToURL('/'), { allowlist }));
+// // to allow work offline
+// registerRoute(new NavigationRoute(createHandlerBoundToURL('/'), { allowlist }));
